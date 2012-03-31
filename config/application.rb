2137a4e41a85c430f5e7c06d4a883ab2997c7b7f
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2012-03-31 15:37:12 +0300

    configure generators

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,12 @@ module ApProject
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.view_specs   false
+      g.controller_specs   false
+      g.helper_specs false
+      g.helper false
+    end
   end
 end
