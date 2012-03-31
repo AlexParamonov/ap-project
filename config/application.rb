f75c18b949c44b8f2215676da95d839e339202af
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2012-03-31 21:38:38 +0300

    Setup CSS structure

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
-#require "sprockets/railtie"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 if defined?(Bundler)
@@ -57,7 +57,7 @@ module ApProject
     # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
